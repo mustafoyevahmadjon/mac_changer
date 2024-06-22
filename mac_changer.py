ba19eac3c5a6a51ac8076e7499386fcc3155fb37
Mustafoyev Axmadjon <mustafayevaxmadjon@gmail.com>
2024-06-22 13:37:57 +0500

    using variables for the  mac changer project

--- mac_changer.py
+++ mac_changer.py
@@ -3,10 +3,10 @@ import subprocess
 interface = "eth0"
 new_mac = "00:22:22:22:22:22"
 
-print("[+] Changing MAC address for " + interface + "to  new_mac" )
+print("[+] Changing MAC address for " + interface + "to " + new_mac )
 
-# subprocess.call("ifconfig")
-# subprocess.call("ifconfig eth0 down", shell=True)
-# subprocess.call("ifconfig eth0 hw ether 88:77:66:55:44:33", shell=True)
-# subprocess.call("ifconfig eth0 up", shell=True)
-# subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig")
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call("ifconfig", shell=True)
