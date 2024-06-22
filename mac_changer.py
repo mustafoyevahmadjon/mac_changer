70a5f041fe045e4444e557a35e35d224dd8e4ad9
Mustafoyev Axmadjon <mustafayevaxmadjon@gmail.com>
2024-06-22 13:26:02 +0500

    variables and strings

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,12 @@
 import subprocess
 
-subprocess.call("ifconfig")
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 88:77:66:55:44:33", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
-subprocess.call("ifconfig", shell=True)
+interface = "eth0"
+new_mac = "00:22:22:22:22:22"
+
+print("[+] Changing MAC address for " + interface + "to  new_mac" )
+
+# subprocess.call("ifconfig")
+# subprocess.call("ifconfig eth0 down", shell=True)
+# subprocess.call("ifconfig eth0 hw ether 88:77:66:55:44:33", shell=True)
+# subprocess.call("ifconfig eth0 up", shell=True)
+# subprocess.call("ifconfig", shell=True)
