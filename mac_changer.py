32992de13d411b64b332f942cc1d6f2e7244aa50
Mustafoyev Axmadjon <mustafayevaxmadjon@gmail.com>
2024-06-22 14:27:00 +0500

    Working with user input

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,10 @@
 import subprocess
 
-interface = input("enter the interface you want to change > ")
-new_mac = input("enter new MAC Adress > ")
+interface = raw_input("enter the interface you want to change > ")
+new_mac = raw_input("enter new MAC Adress > ")
 
 print("[+] Changing MAC address for " + interface + "to " + new_mac )
 
-subprocess.call("ifconfig")
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-subprocess.call("ifconfig", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
