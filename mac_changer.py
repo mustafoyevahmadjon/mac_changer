18fe211df8d73fec14d3f10f92be7ff582a45210
Mustafoyev Axmadjon <mustafayevaxmadjon@gmail.com>
2024-06-22 14:52:59 +0500

    Working with command line arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,10 @@
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i","--interface", dest="interface", help="Interface to change its MAC address")
+parser.parse_args()
 
 interface = raw_input("enter the interface you want to change > ")
 new_mac = raw_input("enter new MAC Adress > ")
